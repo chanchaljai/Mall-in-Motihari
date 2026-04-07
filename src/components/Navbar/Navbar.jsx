d81118e2900a7638dead2b403dd224b0218d887a
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-04-07 09:33:09 +0530

    add search bar in nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import Logo from "../../assets/logo.png";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div>
+      {/* upper navbar */}
+      <div>
+        <div>
+          <div>
+            <a href="#">
+              <img src={Logo} alt="Logo" />
+            </a>
+          </div>
+          {/* Serch bar and order button */}
+        </div>
+        <div className="group">
+          <input
+            type="text"
+            placeholder="search"
+            className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-4 py-2 focus:outline-none focus:border-blue-500"
+          />
+        </div>
+      </div>
+      <div>lower Navbar</div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
